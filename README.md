46b1da339579e26f0e35ce0d778ce29a32d3699e
SG <glue@zhaw.ch>
2019-02-13 15:48:36 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# switch_cell_behavioure_by_chance
+# switch_cell_behaviour_by_chance
+
+Switch behaviour of a cell based on a random number. Here a single biomodule (`GeneralModule`) is used, that runs the simulation 
+step based on the `cell_type`, either `type1behaviour()` or `type2behaviour()` is called in the modules' `Run()`routine.
+
+Simulation starts with `cell_type=1` and is switched to `cell_type=2` by chance (and reverse).
+
+The question is, how to properly use a random number generator inside of the `Run()` function of the BiologyModule (`GeneralModule`).
+
+Further, how to actually get a random number... i.e.:
+```
+// switch behaviour based on a random number
+double myRandNumber = getSomeRandomNumer(); // get some random number from an uniform distribution in [0,1]
+```
+              
--- src/switch_cell_behavioure_by_chance.h
+++ src/switch_cell_behavioure_by_chance.h
@@ -18,22 +18,174 @@
 
 namespace bdm {
 
+// set numer of simulation steps
+    const int simulation_steps = 500; // Time between two simulation steps equals: 0.01hours (default)
+    const double default_cell_diameter = 6; // default diameter of new cells
+
+    namespace bdm {
+
+// 1. Define general cell Module
+        struct GeneralModule : public BaseBiologyModule {
+            GeneralModule() : BaseBiologyModule(gAllEventIds) {}
+
+            /// Empty default event constructor, because GrowthModule does not have state.
+            template <typename TEvent, typename TBm>
+            GeneralModule(const TEvent& event, TBm* other, uint64_t new_oid = 0)
+                    : BaseBiologyModule(event, other, new_oid) {}
+
+            template <typename T, typename TSimulation = Simulation<>>
+            void Run(T* cell) {
+
+              // read current cell type
+              int current_cell_type = cell->GetCellType();
+
+              // run cell behaviour depending on cell type
+              if (current_cell_type==1){
+                type1behaviour(cell);
+              } else if (current_cell_type==2){
+                type2behaviour();
+              } else {
+                std::cout << "Warning/Error: no behaviour defined for cell of type: " << current_cell_type << std::endl;
+              }
+            }
+
+            // define behaviour of cell type S1
+            template <typename T, typename TSimulation = Simulation<>>
+            void type1behaviour(T* cell){
+
+              // cell differentiation
+              runCellCycleDiffStep(cell); // grow cell
+
+              // switch behaviour based on a random number
+              double myRandNumber = getSomeRandomNumer(); // get some random number from an uniform distribution in [0,1]
+              if (myRandNumber <= 0.5) ) {
+                cell->SetCellType(2);
+                cell->SetCellColor(1);
+//                std::cout << "switch to cell type 2" << std::endl;
+              }
+
+            }
+
+            // define behaviour of cell type S2
+            void type2behaviour(){
+              // yet ... do nothing
+
+              // switch behaviour based on a random number
+              double myRandNumber = getSomeRandomNumer(); // get some random number from an uniform distribution in [0,1]
+
+              // switch behaviour based on a random number
+              if (myRandNumber <= 0.5) ) {
+                cell->SetCellType(1);
+                cell->SetCellColor(0);
+//                std::cout << "switch to cell type 2" << std::endl;
+              }
+
+
+            }
+
+            // run cell differentiation step (used in type1)
+            template <typename T, typename TSimulation = Simulation<>>
+            void runCellCycleDiffStep(T* cell){
+              if (cell->GetDiameter() < cell->GetMaxDiam()) { // max_diam not reached
+                cell->SetDiameter(cell->GetDiameter() + 0.2); // grow cell diameter by adding some number
+              } else { // max diam reached -> divide cell
+                auto &&daughter = cell->Divide();
+                daughter->SetDiameter(default_cell_diameter);  // init daughter with default diameter
+              }
+            }
+
+        private:
+            BDM_CLASS_DEF_NV(GeneralModule, 1);
+        };
+
+
+// Define new cell (BDM_SIM_OBJECT) which extends Cell by adding extra data
+        BDM_SIM_OBJECT(MyCell, Cell) {  // our object extends the Cell object
+// create the header with our new data member
+        BDM_SIM_OBJECT_HEADER(MyCell, Cell, 1, cell_type_, cell_color_, max_diam_);
+
+    public:
+        MyCellExt() {}
+        explicit MyCellExt(const std::array<double, 3>& position) : Base(position) {
+          // init cell with type 1, color 0 and max_diam = 10
+          cell_type_[kIdx] = 1;
+          cell_color_[kIdx] = 0;
+          max_diam_[kIdx] = 10;
+        }
+
+        /// If MyCell divides, daughter 2 copies the data members from the mother
+        template <typename TMother>
+        MyCellExt(const CellDivisionEvent& event, TMother* mother)
+                : Base(event, mother) {
+          cell_type_[kIdx] = mother->cell_type_[mother->kIdx];
+          cell_color_[kIdx] = mother->cell_color_[mother->kIdx];
+          max_diam_[kIdx] = mother->max_diam_[mother->kIdx];
+        }
+
+        /// If a cell divides, daughter keeps the same state from its mother.
+        template <typename TDaughter>
+        void EventHandler(const CellDivisionEvent& event, TDaughter* daughter) {
+          Base::EventHandler(event, daughter);
+        }
+
+        // getter and setter for our new data member
+        void SetCellType(int cell_type) { cell_type_[kIdx] = cell_type; }
+        int GetCellType() const { return cell_type_[kIdx]; }
+
+        void SetCellColor(int cell_color) { cell_color_[kIdx] = cell_color; }
+        int GetCellColor() const { return cell_color_[kIdx]; }
+
+        void SetMaxDiam(double max_diam) { max_diam_[kIdx] = max_diam; }
+        double GetMaxDiam() const { return max_diam_[kIdx]; }
+
+    private:
+        // declare new data member and define their type
+        // private data can only be accessed by public function and not directly
+        vec<int> cell_type_;
+        vec<int> cell_color_;
+        vec<double> max_diam_;
+    };
+
+
 // Define compile time parameter
-BDM_CTPARAM() { BDM_CTPARAM_HEADER(); };
+    BDM_CTPARAM() {
+      BDM_CTPARAM_HEADER();
+      using SimObjectTypes = CTList<MyCell>;  // use MyCell object
+      // Override default BiologyModules for Cell
+      BDM_CTPARAM_FOR(bdm, MyCell) { using BiologyModules = CTList<GeneralModule>; };
 
-inline int Simulate(int argc, const char** argv) {
-  Simulation<> simulation(argc, argv);
+    };
 
-  // Define initial model - in this example: single cell at origin
-  auto* rm = simulation.GetResourceManager();
-  auto&& cell = rm->New<Cell>(30);
+    inline int Simulate(int argc, const char** argv) {
+      // set parameters of the simulation
+      auto set_param = [](auto* param) {
+          param->bound_space_ = true;
+          param->min_bound_ = -50;
+          param->max_bound_ = 50;  // plate of 100*100
+          param->run_mechanical_interactions_ = true;
+      };
 
-  // Run simulation for one timestep
-  simulation.GetScheduler()->Simulate(1);
+      Simulation<> simulation(argc, argv, set_param);
 
-  std::cout << "Simulation completed successfully!" << std::endl;
-  return 0;
-}
+      // Define initial model - in this example: single cell at origin
+      auto* rm = simulation.GetResourceManager();
+
+      // create a structure to contain cells
+      auto* cells = rm->template Get<MyCell>();
+      // allocate the correct number of cell in our cells structure before
+      cells->reserve(1);
+
+      MyCell cell({0, 0, 10});
+      cell.SetDiameter(default_cell_diameter);
+      cell.AddBiologyModule(GeneralModule());
+      cells->push_back(cell);  // put the created cell in cells structure
+      cells->Commit();  // commit cells
+
+      // Run simulation
+      simulation.GetScheduler()->Simulate(simulation_steps);
+      std::cout << "Simulation completed successfully!" << std::endl;
+      return 0;
+    }
 
 }  // namespace bdm
 
